d0efac508398235253deb8ddb0d60819508b5f23
Esonic <tariksahin085@gmail.com>
2022-08-29 11:18:16 +0300

    writing code

---
+++ src/test/java/com/cydeo/tests/day03_cssSelector_xpath/anything.java
@@ -0,0 +1,9 @@
+package com.cydeo.tests.day03_cssSelector_xpath;
+
+public class anything {
+    public static void main(String[] args) {
+
+        System.out.println("Github Practice");
+    }
+
+}
