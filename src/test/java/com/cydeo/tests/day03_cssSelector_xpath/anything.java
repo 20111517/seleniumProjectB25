81a0fe02f05f73134db1a85688e810adc0f46a5e
Esonic <tariksahin085@gmail.com>
2022-08-29 11:20:27 +0300

    writing code added

--- src/test/java/com/cydeo/tests/day03_cssSelector_xpath/anything.java
+++ src/test/java/com/cydeo/tests/day03_cssSelector_xpath/anything.java
@@ -3,7 +3,7 @@ package com.cydeo.tests.day03_cssSelector_xpath;
 public class anything {
     public static void main(String[] args) {
 
-        System.out.println("Github Practice");
+        System.out.println("java test");
     }
 
 }
