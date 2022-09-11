c9380585140eb7d0839e94fbdffb3c9f21c89e8e
Esonic <tariksahin085@gmail.com>
2022-09-11 18:34:40 +0300

    Webtable exercises

--- src/test/java/com/cydeo/utilities/WebtableUtils.java
+++ src/test/java/com/cydeo/utilities/WebtableUtils.java
@@ -1,7 +1,28 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class WebtableUtils {
 
+    public static String returnOrderDate(WebDriver driver, String customerName) {
+        String Locator = ("//td[.='" + customerName + "']/following-sibling::td[3]");
+        WebElement customerOrderDate = driver.findElement(By.xpath(Locator));
+        return customerOrderDate.getText();
+    }
+
+public static void orderVerify(WebDriver driver,String customerName,String expectedOrderDate){
+    String Locator = "//td[.='" + customerName + "']/following-sibling::td[3]";
+
+        WebElement customerDatacell= driver.findElement(By.xpath(Locator));
+        String actualOrderDate=customerDatacell.getText();
+    Assert.assertEquals(actualOrderDate,expectedOrderDate);
+}
+
+
+}
 
     //Method #1 info:
     //• Name: returnOrderDate ()
@@ -22,4 +43,4 @@ public class WebtableUtils {
 
 
 
-}
+
